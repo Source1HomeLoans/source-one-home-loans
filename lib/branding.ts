3827ccede257187672732fb062f51cf604591186
David Bates <david@sourceonehomeloans.com>
2026-08-02 12:48:38 -0700

    Add branding configuration

---
+++ lib/branding.ts
@@ -0,0 +1,28 @@
+export const companyName = "Source One Home Loans";
+
+export const tagline = "Your Goal. Our Focus.";
+
+export const website = "https://www.sourceonehomeloans.com";
+
+export const phone = "(469) 310-0042";
+
+export const companyNMLS = "2812359";
+
+export const primaryColor = "#071B35";
+
+export const secondaryColor = "#D4A017";
+
+export const fontPrimary = "Playfair Display";
+
+export const fontSecondary = "Inter";
+
+export const logoPath = "/images/source-one-logo.png";
+
+export const equalHousingLogoPath = "/images/equal-housing-opportunity.png";
+
+export const socialLinks = {
+  facebook: "https://www.facebook.com/profile.php?id=61592025545198",
+  instagram: "https://www.instagram.com/sourceonehomeloans/",
+  linkedin: "https://www.linkedin.com/in/dbates4800",
+} as const;
+
